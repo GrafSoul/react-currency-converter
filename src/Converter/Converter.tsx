6f1dc261a2d7d0741585ac977d28321a1e7b44e2
GrafSoul <grafsoul@gmail.com>
2022-03-07 22:50:02 +0500

    feat: Added fake content for main components.

--- src/Converter/Converter.tsx
+++ src/Converter/Converter.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+import classes from "./Converter.module.scss";
+
 const Converter: React.FC = () => {
-  return <div>Converter</div>;
+  return (
+    <div className={classes.converter}>
+      <form className={classes.converterForm}>
+        <label htmlFor="converter" className={classes.labelConverter}>
+          Введите текст по шаблону "15 usd in rub" указав свою сумму и любые из
+          существующих валют
+        </label>
+        <input
+          type="text"
+          id="converter"
+          name="converter"
+          className={classes.inputConverter}
+        />
+        <button className={classes.btnConverter}>Получить</button>
+      </form>
+
+      <div className={classes.converterResult}>
+        Результат: 15 USD = 100000 RUB
+      </div>
+    </div>
+  );
 };
 
 export default Converter;
--- src/Currencies/Currencies.tsx
+++ src/Currencies/Currencies.tsx
@@ -1,7 +1,38 @@
 import React from "react";
 
+import classes from "./Currencies.module.scss";
+
 const Currencies: React.FC = () => {
-  return <div>Currencies</div>;
+  return (
+    <div className={classes.currencies}>
+      <form className={classes.currencyForm}>
+        <label htmlFor="currency" className={classes.labelCurrency}>
+          Выберите базовую валюту
+        </label>
+
+        <select
+          className={classes.selectCurrency}
+          name="currency"
+          id="currency"
+        >
+          <option value="RUB">RUB</option>
+          <option value="USD">USD</option>
+          <option value="JPY">JPY</option>
+          <option value="CAD">CAD</option>
+          <option value="DKK">DKK</option>
+        </select>
+      </form>
+
+      <div>
+        <ul className={classes.currenciesList}>
+          <li>1 USD = 63.49 RUB</li>
+          <li>1 USD = 63.49 RUB</li>
+          <li>1 USD = 63.49 RUB</li>
+          <li>1 USD = 63.49 RUB</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Currencies;
