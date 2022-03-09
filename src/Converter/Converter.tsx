4076fc47b739726a3335db329f674e8652205c4a
GrafSoul <grafsoul@gmail.com>
2022-03-09 22:46:34 +0500

    feat: Added layout for Converter component.

--- src/Converter/Converter.tsx
+++ src/Converter/Converter.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './Converter.module.scss';
 
 const Converter: React.FC = () => {
+    const [textForConvert, setTextForConvert] = useState<string>('');
+
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(textForConvert.split(' '));
+    };
+
     return (
         <div className={classes.converter}>
-            <form className={classes.converterForm}>
+            <form className={classes.converterForm} onSubmit={onSubmit}>
                 <label htmlFor="converter" className={classes.labelConverter}>
-                    Введите текст по шаблону 15 usd in rub указав свою сумму и любые из существующих
-                    валют
+                    Введите текст по шаблону -{' '}
+                    <span className={classes.textTemplate}>15 usd in rub</span>, <br />
+                    указав сумму и любые из существующих валют
                 </label>
                 <input
                     type="text"
                     id="converter"
                     name="converter"
+                    value={textForConvert}
+                    onChange={(e) => setTextForConvert(e.target.value)}
                     className={classes.inputConverter}
                 />
                 <button className={classes.btnConverter}>Получить</button>
             </form>
 
-            <div className={classes.converterResult}>Результат: 15 USD = 100000 RUB</div>
+            <div className={classes.converterResult}>
+                Результат: 15 <b>USD</b> = 100000 <b>RUB</b>
+            </div>
         </div>
     );
 };
